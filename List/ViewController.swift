12a2353425d4674e2b1524d20603303acfc4719b
Michel Goñi <michel@meep.me>
2020-06-21 09:03:49 +0200

    Initial Commit

---
+++ List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Miguel Goñi on 21/06/2020.
+//  Copyright © 2020 Miguel Goñi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
