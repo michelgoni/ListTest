98d065ab9396530d9f1ac554fa1b96bdb8c9ae0f
Michel Goñi <michel@meep.me>
2020-08-03 18:51:59 +0200

    Adding some tests in selected elements viewController

--- ListTests/ViewControllerTest/SelectedContactsViewControllerTest.swift
+++ ListTests/ViewControllerTest/SelectedContactsViewControllerTest.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+import RxSwift
+import RxCocoa
 import Quick
 import Nimble
 import Action
@@ -14,6 +16,70 @@ import Action
 
 class SelectedContactsViewControllerTest: QuickSpec {
 
-  
+    override func spec() {
+        describe("Selected contacts viewController test implementation") {
+            var sut: SelectedContactsViewController!
+            var nav: UINavigationController!
+            var viewModel: MockSelectedContactsViewModel!
+            var tableView: UITableView!
+            
+            beforeEach {
+                viewModel = MockSelectedContactsViewModel()
+                nav = Scene.selectedContacts(viewModel).viewController() as? UINavigationController
+                sut = nav.viewControllers.first as? SelectedContactsViewController
+                sut.loadViewIfNeeded()
+                tableView = sut.tableView
+            }
+            
+            it("Doesn´t load nil Table View for selected contacts") {
+                expect(sut.tableView).notTo(beNil())
+            }
+            
+            it("Doesn´t load nil Table View after viewDidLoad") {
+                let tableViewisSubview = sut.tableView.isDescendant(of: sut.view)
+                expect(tableViewisSubview).notTo(beNil())
+            }
+            
+            it("Works tableView number of rows is working") {
+                let _ = viewModel.contacts
+                expect(sut.tableView.numberOfRows(inSection: 0)) == 2
+            }
+            
+            it("Works TableView number of sections") {
+                expect(sut.tableView.numberOfSections) == 1
+            }
+            
+            it("returns a custom cell") {
+                let _ = viewModel.contacts
+                
+                let cell = sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+                expect(cell).to(beAKindOf(ContactsTableViewCell.self))
+            }
+        }
+    }
+    
+    class MockSelectedContactsViewModel: DetailContactsViewModel {
+        
+        var contacts = ContactsFake.contactsSelected
+        
+        var coordinator = SceneCoordinator()
+        
+        lazy var resetContacts: CocoaAction = { _ in
+            
+            CocoaAction { _ in
+                return .empty()
+            }
+        }(self)
+        
+        lazy var dismiss: CocoaAction = { _ in
+            CocoaAction { _ in
+                return .empty()
+            }
+        }(self)
+        
+        
+    }
+    
+    
 
 }
