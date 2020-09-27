121b5e28021d88ac0d6872b67341cb3e96c872cc
Michel Goñi <michelgoni@MBP-de-Michel.lan>
2020-09-27 19:49:32 +0200

    Added Driver for button and title selection

--- List/App/Scenes/Main/MainContactList/ListContactsViewController.swift
+++ List/App/Scenes/Main/MainContactList/ListContactsViewController.swift
@@ -14,7 +14,7 @@ import NSObject_Rx
 import RxCocoa
 import TransportsUI
 
- public class ListContactsViewController: BaseViewController {
+public class ListContactsViewController: BaseViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var selectedButton: UIButton!
@@ -44,9 +44,9 @@ import TransportsUI
         
         
         data.bind(to:
-            tableView.rx.items(
-                cellIdentifier: ContactsTableViewCell.identifier,
-                cellType: ContactsTableViewCell.self)
+                    tableView.rx.items(
+                        cellIdentifier: ContactsTableViewCell.identifier,
+                        cellType: ContactsTableViewCell.self)
         ) { _, model, cell in
             cell.setup(with: model)
         }
@@ -77,7 +77,7 @@ import TransportsUI
     }
     
     private func bindTitle() {
-
+        
         var value = ""
         viewModel.updatedContacts.elements.flatMap { contacts -> Observable<String> in
             switch contacts.filter({ $0.isSelected}).count {
@@ -90,39 +90,40 @@ import TransportsUI
             }
             
             return .just(value)
-        }.bind(to: selectedButton.rx.title()).disposed(by: rx.disposeBag)
+        }.asDriver(onErrorJustReturn: "")
+        .drive( selectedButton.rx.title())
+        .disposed(by: rx.disposeBag)
     }
     
     
     func bindButton() {
         
-       let selected =  viewModel.updatedContacts.elements.flatMap { elements -> Observable<Bool> in
+        let selected =  viewModel.updatedContacts.elements.flatMap { elements -> Observable<Bool> in
             return .just(!elements.filter({ $0.isSelected}).isEmpty)
         }.startWith(false)
+        .asDriver(onErrorJustReturn: false)
         
-        selected.subscribe(onNext: { [weak self] (selected) in
-            self?.selectedButton.isEnabled = selected
-            self?.selectedButton.backgroundColor = selected ? .primary : .primaryDisabled
-        }).disposed(by: rx.disposeBag)
-        
+        selected.drive(selectedButton.rx.isEnabled).disposed(by: rx.disposeBag)
+        selected.drive {self.selectedButton.backgroundColor = $0 ? .primary : .primaryDisabled}
+        .disposed(by: rx.disposeBag)
     }
     
     private func bindActivityIndicator() {
-                
+        
         Observable.merge(viewModel.getContacts.executing)
             .subscribe(onNext: { [weak self] isLoading in
-            isLoading ? self?.showLoading() : self?.hideLoading()
-        }).disposed(by: rx.disposeBag)
+                isLoading ? self?.showLoading() : self?.hideLoading()
+            }).disposed(by: rx.disposeBag)
     }
     
     private func bindSelectButton() {
-                
+        
         Observable.merge(viewModel.getContacts.executing)
             .subscribe(onNext: { [weak self] isLoading in
                 self?.selectedButton.isEnabled = isLoading
                 self?.selectedButton.backgroundColor = .primaryDisabled
                 self?.selectedButton.setTitle("", for: .normal)
-        }).disposed(by: rx.disposeBag)
+            }).disposed(by: rx.disposeBag)
     }
     
     
