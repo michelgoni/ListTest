d41ccf1f582eaf8217324746b098ee51ead48642
Michel Goñi <michelgoni@MBP-de-Michel.lan>
2020-12-20 19:43:33 +0100

    Added new appLaunch implementation

--- List/App/AppLaunch/Application.swift
+++ List/App/AppLaunch/Application.swift
@@ -11,15 +11,11 @@ import UIKit
 final class Application {
 
     func start(with window: UIWindow) {
-        
-//        let contactsApiService = ContactsApiServiceImplm()
-//        let repository = ContactsRepositoryImplm(contactsApiService: contactsApiService)
-//        let contactsUsecase = ContactsUseCaseImplm(repository:repository)
-//        
-//        let sceneCoordinator = SceneCoordinator(window: window)
-//        let viewModel = ContactsViewModelImplm(useCase: contactsUsecase, coordinator: sceneCoordinator)
-//        let firstScene = Scene.contacts(viewModel)
-//        sceneCoordinator.transition(to: firstScene, type: .root)
+      
+        let sceneCoordinator = SceneCoordinator(window: window)
+        let viewModel = ContactsViewModelImplm(useCase: DomainContainerDI.shared.contactsUseCase, coordinator: sceneCoordinator)
+        let firstScene = Scene.contacts(viewModel)
+        sceneCoordinator.transition(to: firstScene, type: .root)
 
     }
 }
