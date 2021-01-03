82767adf71917058dda3c5048a60837e83b4770e
Michel Goñi <michelgoni@MBP-de-Michel.lan>
2021-01-03 19:13:50 +0100

    removed non neccesary pods

--- Pods/Target Support Files/Pods-List/Pods-List-frameworks.sh
+++ Pods/Target Support Files/Pods-List/Pods-List-frameworks.sh
@@ -187,7 +187,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwiftExt/RxSwiftExt.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/TransportationApiClient/TransportationApiClient.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TransportsUI/TransportsUI.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -203,7 +202,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwiftExt/RxSwiftExt.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/TransportationApiClient/TransportationApiClient.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TransportsUI/TransportsUI.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
