6e253f1edc28fffb8abbe5b04310f37f5a248ab8
Michel Goñi <michel.goni@katoo.app>
2022-02-21 19:19:25 +0100

    Added viewcontroller test

--- ListTests/ViewControllerTest/ContactsViewControllerTest.swift
+++ ListTests/ViewControllerTest/ContactsViewControllerTest.swift
@@ -18,188 +18,273 @@ import DomainLayer
 
 class ContactsViewControllerTest: QuickSpec {
     
+    var sut: ListContactsViewController!
+    
     override func spec() {
         
         describe("Testing visual elements in Contacts view controller") {
             
-            var sut: ListContactsViewController!
-            var nav: UINavigationController!
-            var viewModel: ContactsMockViewModel!
-            var tableView: UITableView!
-            
             beforeEach {
-                viewModel  = ContactsMockViewModel()
-                nav = Scene.contacts(viewModel).viewController() as? UINavigationController
-                sut = nav.viewControllers.first as? ListContactsViewController
+                self.sut = self.makeSut()
                 
-                sut.loadViewIfNeeded()
-                tableView = sut.tableView
             }
             
-            it("Table view is not nil") {
-                expect(sut.tableView).notTo(beNil())
-            }
-            
-            it("Select contacts button is not nil") {
-                expect(sut.selectedButton).notTo(beNil())
-            }
-            
-            it("Select button is not nil after viewDidLoad") {
-                let selectButtonIsSubView = sut.selectedButton.isDescendant(of: sut.view)
-                expect(selectButtonIsSubView).notTo(beNil())
-            }
-            
-            it("Table view is not nil after viewDidLoad") {
-                let tableViewisSubview = sut.tableView.isDescendant(of: sut.view)
-                expect(tableViewisSubview).notTo(beNil())
-            }
-            
-            it("TableView number is rows is working") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                expect(sut.tableView.numberOfRows(inSection: 0)) == 2
-            }
-            
-            it("TableView number of sections is only one") {
-                expect(sut.tableView.numberOfSections) == 1
-            }
-            
-            it("Tableview returns custom cell") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                
-                let cell = sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0))
-                expect(cell).to(beAKindOf(ContactsTableViewCell.self))
-            }
-            
-            it("Set up for cell is properly called") {
-                
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                sut.tableView.register(MockContactCell.self, forCellReuseIdentifier: MockContactCell.identifier)
-                let cell = sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockContactCell
-                
-                if let element = cell.element {
-                    viewModel.getContacts.elements.map{$0.first?.name}
-                        .subscribe(onNext: { (name) in
-                        expect(element.name) == name ?? ""
-                    }).disposed(by: self.rx.disposeBag)
+            describe("In the ListViewcontroller") {
+                context("when page is loaded") {
+                    it("the table view is not nil") {
+                        expect( self.sut.tableView).notTo(beNil())
+                    }
                 }
             }
             
-            it("Disclosure indicator changes after pressing a contact") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                  sut.tableView.register(MockContactCell.self, forCellReuseIdentifier: MockContactCell.identifier)
-                
-                sut.selectedButton.sendActions(for: .touchUpInside)
-                guard let cell = sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? MockContactCell else {
-                    fail()
-                    return
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the select contacts button is not nil") {
+                        expect( self.sut.selectedButton).notTo(beNil())
+                    }
                 }
-
-                expect(cell.accessoryType) != .checkmark
             }
             
-            it("Modifies background button color after selecting one contact") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                
-                sut.tableView.delegate?.tableView?(tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
-                expect(sut.selectedButton.backgroundColor) == .primary
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the select contacts button is not nil") {
+                        expect( self.sut.selectedButton).notTo(beNil())
+                    }
+                }
+            }
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the select button is not nil after viewDidLoad") {
+                        let selectButtonIsSubView =  self.sut.selectedButton.isDescendant(of:  self.sut.view)
+                        expect(selectButtonIsSubView).notTo(beNil())
+                    }
+                }
+            }
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("The table view is not nil after viewDidLoad") {
+                        let tableViewisSubview =  self.sut.tableView.isDescendant(of:  self.sut.view)
+                        expect(tableViewisSubview).notTo(beNil())
+                    }
+                }
             }
             
-            it("Button is enabled after pressing one contact") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                
-                sut.tableView.delegate?.tableView?(tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
-                expect(sut.selectedButton.isEnabled).to(beTrue())
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("Table view is not nil after viewDidLoad") {
+                        let tableViewisSubview =  self.sut.tableView.isDescendant(of:  self.sut.view)
+                        expect(tableViewisSubview).notTo(beNil())
+                    }
+                }
             }
-            
-            it("Button is not enabled when loading table view") {
-                let _ = Observable.merge(viewModel.getContacts.elements,
-                                         viewModel.updatedContacts.elements)
-                
-                expect(sut.selectedButton.isEnabled).notTo(beTrue())
-            }
-            
-            it("Button text is modified after selecting one contact") {
-                
-                sut.viewModel.updatedContacts.inputs.onNext((contact: ContactsFake.contactSelected, contacts: ContactsFake.contactsSelected))
-                expect(sut.selectedButton.titleLabel?.text) == "1 element selected"
-            }
-            it("TableView number is rows is working when query string is passed") {
-                sut.viewModel.searchContacts.inputs.onNext("Hulk")
-                expect(tableView.numberOfRows(inSection: 0)).notTo(beNil())
-            }
-            
-            
-            class MockContactCell: ContactsTableViewCell {
-                
-                let identifier = "MockContactCell"
-                var element: ContactRepresentable?
-                var accesoryTypeElement = ""
-                
-                override func setup(with data: Any) {
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the tableView number of rows is working") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+                        expect( self.sut.tableView.numberOfRows(inSection: 0)) == 2
+                    }
                     
-                    guard let contactElement = data as? ContactRepresentable else {return}
-                    accessoryType = contactElement.isSelected ? .checkmark : .none
-                    element = contactElement
                 }
             }
-            
-            class ContactsMockViewModel: ContactsViewModel {
-                
-                
-                var offset: Int = 10
-                
-           
-                
-                lazy var searchContacts: Action<String, [Contact]> = { _ in
-                    Action<String, [Contact]> { this in
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the tableView number of sections is only one") {
+                        expect(self.sut.tableView.numberOfSections) == 1
+                    }
+                    
+                }
+            }
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the tableview returns custom cell") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
                         
-                        return .just(ContactsFake.searchContacts)
+                        let cell = self.sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+                        expect(cell).to(beAKindOf(ContactsTableViewCell.self))
                     }
                     
-                }(self)
-
-                lazy var getContacts: Action<Void, [Contact]> = { _ in
-                    Action <Void, [Contact]> {
-                       
-                        return .just(ContactsFake.contacts)
-                    }
-                }(self)
-                
-                lazy var loadNextPageContacts: Action<Void, [Contact]> = { _ in
-                    Action<Void, [Contact]> { this in
+                }
+            }
+            
+            describe("In the ListViewcontroller") {
+                context("when the page is loaded") {
+                    it("the set up for the cell is properly called") {
                         
-                        return .just(ContactsFake.searchContacts)
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+                        self.sut.tableView.register(MockContactCell.self, forCellReuseIdentifier: MockContactCell.identifier)
+                        let cell = self.sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockContactCell
+                        
+                        if let element = cell.element {
+                            self.sut.viewModel.getContacts.elements.map{$0.first?.name}
+                            .subscribe(onNext: { (name) in
+                                expect(element.name) == name ?? ""
+                            }).disposed(by: self.rx.disposeBag)
+                        }
                     }
                     
-                }(self)
-                
-                lazy var updatedContacts: Action<(contact: Contact, contacts: [Contact]), [Contact]> = { _ in
-                    
-                    Action<(contact: Contact, contacts: [Contact]), [Contact]> { _ in
-                        return .just(ContactsFake.contactsSelected)
+                }
+            }
+            
+            describe("In the ListViewcontroller") {
+                context("after pressing a contact") {
+                    it("the disclosure indicator changes") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+                        self.sut.tableView.register(MockContactCell.self, forCellReuseIdentifier: MockContactCell.identifier)
+
+                        self.sut.selectedButton.sendActions(for: .touchUpInside)
+                        guard let cell = self.sut.tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? MockContactCell else {
+                            fail()
+                            return
+                        }
+
+                        expect(cell.accessoryType) != .checkmark
                     }
-                    
-                }(self)
-                
-                lazy var selectedContacts: Action<[Contact], Void> = { _ in
-                    Action<[Contact], Void> { _ in
-                        return .empty()
+
+                }
+            }
+
+            describe("In the ListViewcontroller") {
+                context("when a contact is selected") {
+                    it("modifies background button color") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+
+                        self.sut.tableView.delegate?.tableView?(self.sut.tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+                        expect(self.sut.selectedButton.backgroundColor) == .primary
                     }
-                }(self)
-                
-                lazy var resetContacts: CocoaAction = { _ in
-                    CocoaAction { _ in
-                        return .empty()
+
+                }
+            }
+
+            describe("In the ListViewcontroller") {
+                context(" after pressing one contact") {
+                    it("Button is enabled") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+
+                        self.sut.tableView.delegate?.tableView?(self.sut.tableView, didSelectRowAt: IndexPath(row: 0, section: 0))
+                        expect(self.sut.selectedButton.isEnabled).to(beTrue())
                     }
-                }(self)
-                
+
+                }
+            }
+
+            describe("In the ListViewcontroller") {
+                context("when table view is loaded") {
+                    it("Button is not enabled ") {
+                        let _ = Observable.merge(self.sut.viewModel.getContacts.elements,
+                                                 self.sut.viewModel.updatedContacts.elements)
+
+                        expect(self.sut.selectedButton.isEnabled).notTo(beTrue())
+                    }
+
+                }
+            }
+
+            describe("In the ListViewcontroller") {
+                context("after selecting one contact") {
+                    it("Button text is modified ") {
+
+                        self.sut.viewModel.updatedContacts.inputs.onNext((contact: ContactsFake.contactSelected, contacts: ContactsFake.contactsSelected))
+                        expect(self.sut.selectedButton.titleLabel?.text) == "1 element selected"
+                    }
+
+                }
+            }
+
+            describe("In the ListViewcontroller") {
+                context("when a query string is passed") {
+                    it("tableView number is rows is working ") {
+                        self.sut.viewModel.searchContacts.inputs.onNext("Hulk")
+                        expect(self.sut.tableView.numberOfRows(inSection: 0)).notTo(beNil())
+                    }
+
+                }
+            }
+            afterEach {
+                self.sut = nil
             }
         }
+        
+    }
+    
+    private func makeSut() -> ListContactsViewController {
+        let viewModel  = ContactsMockViewModel()
+        let nav = Scene.contacts(viewModel).viewController() as! UINavigationController
+        let sut = nav.viewControllers.first as! ListContactsViewController
+        
+        sut.loadViewIfNeeded()
+        
+        return sut
+    }
+}
+
+private  class ContactsMockViewModel: ContactsViewModel {
+    
+    
+    var offset: Int = 10
+    
+
+    
+    lazy var searchContacts: Action<String, [Contact]> = { _ in
+        Action<String, [Contact]> { this in
+            
+            return .just(ContactsFake.searchContacts)
+        }
+        
+    }(self)
+
+    lazy var getContacts: Action<Void, [Contact]> = { _ in
+        Action <Void, [Contact]> {
+           
+            return .just(ContactsFake.contacts)
+        }
+    }(self)
+    
+    lazy var loadNextPageContacts: Action<Void, [Contact]> = { _ in
+        Action<Void, [Contact]> { this in
+            
+            return .just(ContactsFake.searchContacts)
+        }
+        
+    }(self)
+    
+    lazy var updatedContacts: Action<(contact: Contact, contacts: [Contact]), [Contact]> = { _ in
+        
+        Action<(contact: Contact, contacts: [Contact]), [Contact]> { _ in
+            return .just(ContactsFake.contactsSelected)
+        }
+        
+    }(self)
+    
+    lazy var selectedContacts: Action<[Contact], Void> = { _ in
+        Action<[Contact], Void> { _ in
+            return .empty()
+        }
+    }(self)
+    
+    lazy var resetContacts: CocoaAction = { _ in
+        CocoaAction { _ in
+            return .empty()
+        }
+    }(self)
+    
+}
+
+private class MockContactCell: ContactsTableViewCell {
+    
+    let identifier = "MockContactCell"
+    var element: ContactRepresentable?
+    var accesoryTypeElement = ""
+    
+    override func setup(with data: Any) {
+        
+        guard let contactElement = data as? ContactRepresentable else {return}
+        accessoryType = contactElement.isSelected ? .checkmark : .none
+        element = contactElement
     }
 }
